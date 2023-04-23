debda4638e962baa890adf36f43180fb907d0a61
wrigh4d <wrigh4d@cmich.edu>
2023-04-23 16:14:07 -0400

    Featured Polls

--- frontend/src/Components/Featured.js
+++ frontend/src/Components/Featured.js
@@ -1,132 +1,90 @@
 import React from "react";
-import records from "../records.json";
 
-const featuredCategories = [
-  { category: "Gaming", polls: [{}] },
-  { category: "Movies", polls: [{}] },
-  { category: "TV", polls: [{}] },
-  { category: "Pop Culture", polls: [{}] },
-];
-const gamingPolls = [];
-const moviePolls = [];
-const tvPolls = [];
-const pcPolls = [];
+// function handleSeeMore({ handleModal, category }) {
+//   let polls;
 
-if (typeof window !== 'undefined') { // Check if we're running in the browser.
-  for (let i = 0; i < records.polls.length; i++) {
-    let currPoll = records.polls[i];
-    
-    switch (currPoll.category) {
-      case "Gaming":
-        if (gamingPolls.length < 5) {
-          // fill up array
-          gamingPolls[gamingPolls.length] = currPoll;
-          break;
-        } else {
-          // check each poll and if the like value is less switch them out
-          for (let x = 0; x < gamingPolls.lenght; x++) {
-            if (gamingPolls[x].likes > currPoll.likes) {
-              gamingPolls[x] = currPoll;
-              break;
-            }
-            break;
-          }
-        }
-        break;
-      case "Movies":
-        if (moviePolls.length < 5) {
-          // fill up array
-          moviePolls[moviePolls.length] = currPoll;
-          break;
-        } else {
-          // check each poll and if the like value is less switch them out
-          for (let x = 0; x < moviePolls.lenght; x++) {
-            if (moviePolls[x].likes > currPoll.likes) {
-              moviePolls[x] = currPoll;
-              break;
-            }
-            break;
-          }
-        }
-        break;
-      case "TV":
-        if (tvPolls.length < 5) {
-          // fill up array
-          tvPolls[tvPolls.length] = currPoll;
-          break;
-        } else {
-          // check each poll and if the like value is less switch them out
-          for (let x = 0; x < tvPolls.lenght; x++) {
-            if (tvPolls[x].likes > currPoll.likes) {
-              tvPolls[x] = currPoll;
-              break;
-            }
-            break;
-          }
-        }
-        break;
-      case "Pop Culture":
-        if (pcPolls.length < 5) {
-          // fill up array
-          pcPolls[pcPolls.length] = currPoll;
-          break;
-        } else {
-          // check each poll and if the like value is less switch them out
-          for (let x = 0; x < pcPolls.lenght; x++) {
-            if (pcPolls[x].likes > currPoll.likes) {
-              pcPolls[x] = currPoll;
-              break;
-            }
-            break;
-          }
-        }
-        break;
-      default:
-        break;
-    }
-  }
+//   const fetchPolls = async () => {
+//     let response = await fetch(
+//       "http://localhost:3000/polls?category=" +
+//         category +
+//         "&_sort=likes&_order=asc"
+//     );
+//     polls = await response.json();
+//   };
 
-  featuredCategories[0].polls = gamingPolls;
-  featuredCategories[1].polls = moviePolls;
-  featuredCategories[2].polls = tvPolls;
-  featuredCategories[3].polls = pcPolls;
-}
+//   (async () => await fetchPolls())();
 
-function Featured({ handleModal }) {
+//   console.log(polls);
+//   return (
+//     <div className="featuredContainer">
+//       <div className="featuredContent">
+//         <div style={{ display: "flex", flexDirection: "row", gap: "15px" }}>
+//           <h1>{category}</h1>
+//         </div>
+//         <div className="featuredPostContainer">
+//           {polls.map((post, index) => (
+//             <div
+//               key={index}
+//               className="featuredPost"
+//               onClick={() => handleModal(post)}
+//             >
+//               <div className="featuredPostTitle">
+//                 <h2>{post.question}</h2>
+//               </div>
 
+//               <div className="featuredPostLikes">
+//                 <h3>{post.likes} Likes</h3>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+function Featured({ handleModal, featuredCategories, isLoading }) {
   return (
     <>
-      <div className="featuredContainer">
-        <div className="featuredContent">
-          {featuredCategories.map((section, index) => (
-            <div key={index}>
-              <div
-                style={{ display: "flex", flexDirection: "row", gap: "15px" }}
-              >
-                <h1>{section.category}</h1>
-                <button id="optionsButton">See More +</button>
-              </div>
-              <div className="featuredPostContainer">
-                {section.polls.map((post, index) => (
-                  <div
-                    key={index}
-                    className="featuredPost"
-                    onClick={() => handleModal(post)}
+      {isLoading ? (
+        <h1 style={{ textAlign: "center", margin: "20px" }}>Is Loading...</h1>
+      ) : (
+        <div className="featuredContainer">
+          <div className="featuredContent">
+            {featuredCategories.map((section, index) => (
+              <div key={index}>
+                <div
+                  style={{ display: "flex", flexDirection: "row", gap: "15px" }}
+                >
+                  <h1>{section.category}</h1>
+                  <button
+                    id="optionsButton"
                   >
-                    <div className="featuredPostTitle">
-                      <h2>{post.question}</h2>
-                    </div>
+                    See More +
+                  </button>
+                </div>
+                <div className="featuredPostContainer">
+                  {section.polls.map((post, index) => (
+                    <div
+                      key={index}
+                      className="featuredPost"
+                      onClick={() => handleModal(handleModal, post)}
+                    >
+                      <div className="featuredPostTitle">
+                        <h2>{post.question}</h2>
+                      </div>
 
-                    <div className="featuredPostLikes">
-                      <h3>{post.likes} Likes</h3>
+                      <div className="featuredPostLikes">
+                        <h3>{post.likes} Likes</h3>
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
