c9a3120e970a4a433a97070b328acd439e49d3ef
Gary Pinsky <kd8tbs@gmail.com>
2023-04-24 17:28:27 -0400

    fixed the bug
    u know

--- frontend/src/Components/SectionManager.js
+++ frontend/src/Components/SectionManager.js
@@ -4,13 +4,7 @@ import CategoryPolls from "./CategoryPolls";
 
 function SectionManager({ handleModal }) {
   const [section, setSection] = useState(false); // false = featured, true = see more
-
-  const featuredPolls = [
-    { category: "Gaming", polls: [{}] },
-    { category: "Movies", polls: [{}] },
-    { category: "TV", polls: [{}] },
-    { category: "Pop Culture", polls: [{}] },
-  ];
+  const [featuredPolls, setFeaturedPolls] = useState([]);
 
   useEffect(() => {
     const fetchFeaturedPolls = async () => {
@@ -32,13 +26,18 @@ function SectionManager({ handleModal }) {
         );
         const pcPolls = await response.json();
 
-        featuredPolls[0].polls = gamingPolls;
-        featuredPolls[1].polls = moviePolls;
-        featuredPolls[2].polls = tvPolls;
-        featuredPolls[3].polls = pcPolls;
+        let tempPolls = [
+          { category: "Gaming", polls: gamingPolls },
+          { category: "Movies", polls: moviePolls },
+          { category: "TV", polls: tvPolls },
+          { category: "Pop Culture", polls: pcPolls },
+        ]
+
+        setFeaturedPolls(tempPolls);
       } catch (err) {
         console.log(err.stack);
       } finally {
+        console.log(featuredPolls)
       }
     };
 
